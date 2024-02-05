da6710ee7f53ffa359be4cc9613cec0f2f186d5e
Arley Souto <macx15@hotmail.com>
2024-02-05 19:22:01 -0300

    feat: finalizado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,31 @@
 import { useEffect, useState } from 'react'
-import { DivContainer, Input } from './styles'
+import { DeviceContent, DivContainer } from './styles'
 import { api } from '../../lib/axios'
 import { AxiosError } from 'axios'
 import { DataDevice } from '../../DTOs/dataDevice'
 import dayjs from 'dayjs'
+import { useSearchParams } from 'react-router-dom'
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import { defaultTheme } from '@/styles/theme'
 
-type Telemetry = {
-  mac: string // Identificador do dispositivo
-  temp_1: number
-  temp_2: number
-  rssi: number
-  date: number
-  compressor_buffer: string
-  evap_fan_buffer: string
-  defrost_buffer: string
-  open_door_buffer: string
-  name: string
-}
-
-const id = '65c1267706b0ff9cb40357d0'
+//const id = '65c1267706b0ff9cb40357d0' para consultar o device pela url basta colocar o ?id=65c1267706b0ff9cb40357d0 depois da barra
 
 export function Home() {
   const [dataDevice, setDataDevice] = useState({} as DataDevice)
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const idDevice = searchParams.get('id')
 
   async function reqDataDevice() {
     try {
-      const response = await api.get(`/installation-meter/${id}`, {
+      const response = await api.get(`/installation-meter/${idDevice}`, {
         headers: {
           'x-access-token':
             'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTU3YTA5YWY5ODFiYTUyODc3ZjAzNiIsImlhdCI6MTY3ODgzODgxM30.F5Icoma-bOswkRmKpjYjmAQrXE32CM9kAQ0D2S0JgPY',
@@ -33,10 +33,10 @@ export function Home() {
       })
 
       setDataDevice(response.data.installationMeter)
-      //setAllDataDevice(response.data.installationMeter)
-      console.log(response.data)
     } catch (error) {
-      console.log(error)
+      if (error instanceof AxiosError) {
+        console.log(error.message)
+      }
     }
   }
 
@@ -46,22 +46,50 @@ export function Home() {
 
   return (
     <main>
-      <div>
-        <div style={{gap: 20 }}>
+      <DivContainer>
+        <DeviceContent>
           <h4>ID do dispositivo: {dataDevice.meter} </h4>
           <h4>Nome: {dataDevice.name}</h4>
-          <h4>Data de ativação: {dayjs(dataDevice.createdAt).format('DD/MM/YYYY')}</h4>
-        <h4>Data de instalação: {dayjs(dataDevice.installDate).format('DD/MM/YYYY')}</h4>
+          <h4>
+            Data de ativação: {dayjs(dataDevice.createdAt).format('DD/MM/YYYY')}
+          </h4>
+          <h4>
+            Data de instalação:{' '}
+            {dayjs(dataDevice.installDate).format('DD/MM/YYYY')}
+          </h4>
           <h4>Local: {dataDevice.comment}</h4>
           <h4>Versão do firmare: {dataDevice.fwVer?.version}</h4>
-          <h4>Força do compressor: {dataDevice.compressor_power ? dataDevice.compressor_power : '-'}</h4>
-          <h4>Descongelar: {dataDevice.defrost_power ? dataDevice.defrost_power : '-'}</h4>
-
-        </div>
+          <h4>
+            Força do compressor:{' '}
+            {dataDevice.compressor_power ? dataDevice.compressor_power : '-'}
+          </h4>
+          <h4>
+            Descongelar:{' '}
+            {dataDevice.defrost_power ? dataDevice.defrost_power : '-'}
+          </h4>
+        </DeviceContent>
         <div>
-         
+          <ResponsiveContainer width="100%" height={240}>
+            <LineChart data={[dataDevice]} style={{ fontSize: 12 }}>
+              <XAxis dataKey="compressor_power" />
+              <YAxis stroke="#888" />
+              <CartesianGrid className="stroke-muted" />
+              <Line
+                type="linear"
+                strokeWidth={2}
+                dataKey="compressor_power"
+                stroke={defaultTheme['red-500']}
+              />
+              <Line
+                type="linear"
+                strokeWidth={2}
+                dataKey="defrost_power"
+                stroke={defaultTheme['green-500']}
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
-      </div>
+      </DivContainer>
     </main>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,8 +2,19 @@ import styled from "styled-components";
 
 export const DivContainer = styled.div`
     display: flex;
-    margin: 30px;
-    gap: 10px;
+    margin: 2.5rem;
+    background:${(theme) => theme.theme['gray-900']};
+    border-radius: 5px;
+    padding: 2rem;
+    flex-direction: column;
+ 
+`
+
+export const DeviceContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-bottom: 2rem;
 `
 
 export const Input = styled.input`
