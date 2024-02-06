4074ce7874c9fa8de55c1090d6de4036579e6260
Arley Souto <macx15@hotmail.com>
2024-02-06 20:45:45 -0300

    feat: adicionado timescale

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,6 +7,7 @@ import dayjs from 'dayjs'
 import { useParams } from 'react-router-dom'
 import data from '../../../data.json'
 import {
+  Brush,
   CartesianGrid,
   Line,
   LineChart,
@@ -53,14 +54,14 @@ export function Home() {
   const dateFormatter = (date: number) => {
     const dateTimes = new Date(date)
 
-    return dayjs(dateTimes).format('DD/MM/YYYY')
+    return dayjs(dateTimes).format('DD/MM/YYYY HH:mm')
   }
 
   const selectedForDate = useMemo(() => {
     return data.map((selecteForDate) => {
-      const date = dayjs(new Date(selecteForDate.date)).format('DD/MM/YYYY')
+      const date = dayjs(new Date(selecteForDate.date)).format('DD/MM/YYYY HH:mm')
       return {
-        date,
+        date:  date,
         temp_1: selecteForDate.temp_1,
         temp_2: selecteForDate.temp_2,
       }
@@ -69,10 +70,14 @@ export function Home() {
 
   const dataFilter =
     selectedForDate.length > 0
-      ? selectedForDate.filter((date) =>
-          date.date.includes(dayjs(dateInitial).format('DD/MM/YYYY'))
-        )
+      ? selectedForDate.filter(
+          (date) =>{
+            return date.date.includes(dayjs(dateInitial).format('DD/MM/YYYY'))
+          }
+            )
       : []
+
+  //console.log(selectedForDate)
 
   return (
     <main>
@@ -116,15 +121,18 @@ export function Home() {
                 left: 20,
                 bottom: 5,
               }}
+              
             >
               <CartesianGrid strokeDasharray="3 3" />
+              <Brush dataKey="0" height={30} stroke="#8884d8" />
               <XAxis
+              
                 dataKey="date"
                 tickFormatter={dateFormatter}
                 minTickGap={5}
               />
               <YAxis stroke="#888" />
-              <CartesianGrid className="stroke-muted" />
+              <CartesianGrid strokeDasharray='3 3' />
               <Line
                 type="monotone"
                 strokeWidth={2}
